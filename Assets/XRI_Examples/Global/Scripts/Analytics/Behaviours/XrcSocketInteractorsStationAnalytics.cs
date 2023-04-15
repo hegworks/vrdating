794c4f15e1eedf48aa6334e8a8a06cf70df08966
Hessam Ghadimi <hess.gha@gmail.com>
2023-04-16 00:22:52 +0330

    SpeechRecognitionManager + first build attempt

--- Assets/XRI_Examples/Global/Scripts/Analytics/Behaviours/XrcSocketInteractorsStationAnalytics.cs
+++ Assets/XRI_Examples/Global/Scripts/Analytics/Behaviours/XrcSocketInteractorsStationAnalytics.cs
@@ -25,24 +25,24 @@ namespace UnityEngine.XR.Content.Interaction.Analytics
 
         void Start()
         {
-            XrcAnalyticsUtils.Register(m_SimpleSocket, new ConnectSocketSimpleObject(), new DisconnectSocketSimpleObject());
-
-            XrcAnalyticsUtils.Register(m_BatterySlotSocket, new ConnectPerlerMachineBattery());
-
-            var grabPerlerBeadParameter = new GrabPerlerBead();
-            foreach (var socket in m_InfinityPegSockets)
-            {
-                foreach (var interactable in socket.interactablesSelected)
-                {
-                    XrcAnalyticsUtils.Register(interactable as XRBaseInteractable, grabPerlerBeadParameter);
-                }
-
-                socket.selectEntered.AddListener(args => XrcAnalyticsUtils.Register(args.interactableObject as XRBaseInteractable, grabPerlerBeadParameter));
-            }
-
-            var connectPerlerBeadParameter = new ConnectPerlerBead();
-            foreach (var gridSocket in m_GridCenter.GetComponentsInChildren<XRSocketInteractor>())
-                XrcAnalyticsUtils.Register(gridSocket, connectPerlerBeadParameter);
+            // XrcAnalyticsUtils.Register(m_SimpleSocket, new ConnectSocketSimpleObject(), new DisconnectSocketSimpleObject());
+            //
+            // XrcAnalyticsUtils.Register(m_BatterySlotSocket, new ConnectPerlerMachineBattery());
+            //
+            // var grabPerlerBeadParameter = new GrabPerlerBead();
+            // foreach (var socket in m_InfinityPegSockets)
+            // {
+            //     foreach (var interactable in socket.interactablesSelected)
+            //     {
+            //         XrcAnalyticsUtils.Register(interactable as XRBaseInteractable, grabPerlerBeadParameter);
+            //     }
+            //
+            //     socket.selectEntered.AddListener(args => XrcAnalyticsUtils.Register(args.interactableObject as XRBaseInteractable, grabPerlerBeadParameter));
+            // }
+            //
+            // var connectPerlerBeadParameter = new ConnectPerlerBead();
+            // foreach (var gridSocket in m_GridCenter.GetComponentsInChildren<XRSocketInteractor>())
+            //     XrcAnalyticsUtils.Register(gridSocket, connectPerlerBeadParameter);
         }
     }
 }
